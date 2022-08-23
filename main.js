e3dcacde03c2b9034923ca0b88aa6bfc7e8e0324
zardoc9688 <105289615+zardoc9688@users.noreply.github.com>
2022-08-23 04:38:15 -0500

    avance

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let header = document.querySelector('.header');
 let hambumgerMenu = document.querySelector('.hamburger-menu');
 
+window.addEventListener('scroll', function(){
+    let windowPosition = window.scrollY>0;
+    header.classList.toggle('active',windowPosition)
+})
+
 hambumgerMenu.addEventListener('click', function () {
     header.classList.toggle('menu-open');
 })
